336c2cdc529562a5f0d8cd50952bf742af57be65
Hwannni <cmhcms1115@naver.com>
2024-06-19 23:48:41 +0900

    2024_06_19

--- Level_0/Day7/Q2.py
+++ Level_0/Day7/Q2.py
@@ -1,2 +1,9 @@
-hello
-hi
+def solution(l, r):
+    result = []
+    for num in range(l, r + 1):
+        str_num = str(num)
+        if all(c in "05" for c in str_num):
+            result.append(num)
+    if not result:
+        return [-1]
+    return result
